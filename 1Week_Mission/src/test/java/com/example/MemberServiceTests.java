37dd38a2d7f2732763bedce2cb246597ba50c9e4
gimminjae <min356812@naver.com>
2022-10-17 16:36:40 +0900

    Fix: 테스트 코드 수정

--- 1Week_Mission/src/test/java/com/example/MemberServiceTests.java
+++ 1Week_Mission/src/test/java/com/example/MemberServiceTests.java
@@ -7,11 +7,15 @@ import com.example.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class MemberServiceTests {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Autowired
     private MemberService memberService;
 
@@ -21,7 +25,6 @@ public class MemberServiceTests {
         MemberDto member = memberService.create("min356812", "qwer1234@", "nickname", "min3568@naver.com");
 
         assertThat(member.getUsername()).isEqualTo("min356812");
-        assertThat(member.getPassword()).isEqualTo("qwer1234@");
     }
 
     //회원 조회 테스트
@@ -30,7 +33,6 @@ public class MemberServiceTests {
         MemberDto member = memberService.getMemberById(1L);
 
         assertThat(member.getUsername()).isEqualTo("min356812");
-        assertThat(member.getPassword()).isEqualTo("qwer1234@");
     }
 
     //회원 정보 수정 테스트
@@ -48,10 +50,9 @@ public class MemberServiceTests {
     void modifyPassword() {
         MemberDto member = memberService.getMemberById(1L);
 
-        assertThat(member.getPassword()).isEqualTo("qwer1234@");
         memberService.modifyPassword(member, "newqwer1234@");
         MemberDto modifiedMember = memberService.getMemberById(1L);
 
-        assertThat(modifiedMember.getPassword()).isEqualTo("newqwer1234@");
+        assertTrue(!modifiedMember.getPassword().equals(passwordEncoder.encode("qwer1234@")));
     }
 }
