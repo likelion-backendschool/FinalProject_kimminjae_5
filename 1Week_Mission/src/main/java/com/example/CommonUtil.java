85792089616a237795fc227694384f7f9d4567f8
gimminjae <min356812@naver.com>
2022-10-19 17:17:27 +0900

    1Week_end

--- 1Week_Mission/src/main/java/com/example/CommonUtil.java
+++ 1Week_Mission/src/main/java/com/example/CommonUtil.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CommonUtil {
+    //렌더링
     public static String markdown(String markdown) {
         Parser parser = Parser.builder().build();
         Node document = parser.parse(markdown);
